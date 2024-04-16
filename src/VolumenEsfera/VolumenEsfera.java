812af54c030b027d4a5ad7ec6839ba8554eca294
Blopez1219 <ada_hack65@hackwomen.dev>
2024-04-15 19:46:41 -0500

    Volumen de esfera
    Ejercicio para calcular el volumen de una esfera

---
+++ src/VolumenEsfera/VolumenEsfera.java
@@ -0,0 +1,19 @@
+package VolumenEsfera;
+
+import java.util.Scanner;
+
+public class VolumenEsfera {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese el radio de la esfera: ");
+        double radio = scanner.nextDouble();
+
+        double volumen = (4.0 / 3.0) * Math.PI * Math.pow(radio, 3);
+
+        System.out.println("El volumen de la esfera es: " + volumen);
+	}
+
+}
